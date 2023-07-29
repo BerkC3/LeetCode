4485b1d4ed50f82855b9bbbf0451e211c343dfc9
BerkC3 <77547402+BerkC3@users.noreply.github.com>
2023-07-29 06:35:04 +0300

    Description, Solution, Efficiency

---
+++ 2553. Separate the Digits in an Array/2553. Separate the Digits in an Array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def separateDigits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i in nums:
+            for j in str(i):
+                res.append(int(j))
+        return res
